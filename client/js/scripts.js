c76384e76ae3539e4d36d57b118cd5e6fc438d7b
TristanLobaugh <ttrader_atl@yahoo.com>
2016-06-05 03:55:31 -0400

    moved all functionality over to server side

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,45 +1,57 @@
 $(document).ready(function(){
+	var orbs;
+	var players;
+	var wHeight = $(window).height();
+	var wWidth = $(window).width();
+	var player = {};
+	player.zoom = 1.5;
 
-// SOCKET.IO STUFF
-	var socketio = io.connect();
-	socketio.on("message_to_client", function(data){
-		console.log(data.message);
-	});
+	var startorbs = 1000;
 
-	var startPlayers = 25;
-	var startPoints = 1000;
-	var speed = 4;
-	var xVector = 0;
-	var yVector = 0;
-	var zoom = 1.5;
-
-	var points = [];
-	var players = [];
-	
-	var wHeight = $(window).height() - 10;
-	var wWidth = $(window).width() - 10;
-
-	var worldWidth = 5000;
-	var worldHeight = 5000;
 
 
 	var canvas = document.getElementById("the-canvas");
 	var context = canvas.getContext("2d");
 	canvas.width = wWidth;
 	canvas.height = wHeight;
-	function newPlayer(num){
-		for(var i = 0; i < num; i++){
-			players.push(new Player());
-		}
-	}
 
-	function Player(){
-		this.locX = Math.floor((Math.random()*worldWidth) + 10); 
-		this.locY = Math.floor((Math.random()*worldHeight) + 10);
-		this.speed = speed;
-		this.radius = 6;
-		this.color = getRandomColor();
-	}
+	// SOCKET.IO STUFF
+	var socket = io.connect();
+	socket.on("message_to_client", function(data){
+		console.log(data.message);		
+	});
+
+	socket.on("init", function(data){
+		player = data.init;
+		orbs = data.orbs;
+		players = data.players;
+		console.log(player.id);
+		draw();
+	});
+
+	setInterval(function(){
+		socket.emit("tick",{
+			player: player
+		});
+	}, 1000/60);
+
+	socket.on("tock", function(data){
+		players = data.players;
+		player.locX = data.x;
+		player.locY = data.y;
+	});
+
+	socket.on("collision", function(data){
+		players = data.players;
+		// orbs = data.orbs;
+		for(var i = 0; i < players.length; i++){
+			if(players[i].id == player.id){
+				player = players[i];
+			}
+		}
+	});
+
+
 	// Currently not used, but should stop the camera from over scrolling
 	// function clamp(value, min, max){
 	//     if(value < min) return min;
@@ -48,181 +60,65 @@ $(document).ready(function(){
 	// }
 
 	function draw(){
-		canvas.style.width = (wWidth * zoom) +"px";
-		canvas.style.height = (wHeight * zoom) +"px";
+		canvas.style.width = (wWidth * player.zoom) +"px";
+		canvas.style.height = (wHeight * player.zoom) +"px";
 		context.setTransform(1,0,0,1,0,0);//reset the transform matrix as it is cumulative
 		context.clearRect(0, 0, canvas.width, canvas.height);//clear the viewport AFTER the matrix is reset
 
 		//Clamp the camera position to the world bounds while centering the camera around the player                                             
-		var camX = -players[0].locX + canvas.width/(2*zoom);
-		var camY = -players[0].locY + canvas.height/(2*zoom);
-
+		var camX = -player.locX + canvas.width/(2*player.zoom);
+		var camY = -player.locY + canvas.height/(2*player.zoom);
 		context.translate(camX, camY);
 
-	// DRAW POINTS
-		for(var i = 0; i < points.length; i++){
+	// DRAW ORBS
+		for(var i = 0; i < orbs.length; i++){
 			context.beginPath();
-			context.fillStyle = points[i].color;
-			context.arc(points[i].locX, points[i].locY, points[i].radius, 0, Math.PI*2);
+			context.fillStyle = orbs[i].color;
+			context.arc(orbs[i].locX, orbs[i].locY, orbs[i].radius, 0, Math.PI*2);
 			context.fill();
 		}
 	// DRAW PLAYERS
 		for (var i = 0; i < players.length; i++){
-			if(players[i].locX < 10 || players[i].locX > worldWidth){
-				players[i].speed = -players[i].speed;
-			}else if(players[i].locY < 10 || players[i].locY > worldHeight){
-				players[i].speed = -players[i].speed;
-			}
 			context.beginPath();
 			context.fillStyle = players[i].color;
 			context.arc(players[i].locX, players[i].locY, players[i].radius, 0, Math.PI*2);
 			context.fill();
 			context.lineWidth = 5;
-			if(i != 0){
+			if(players[i].id != player.id){
 				context.strokeStyle = '#ff0000';
-				players[i].locX += players[i].speed;
-				players[i].locY += players[i].speed;
 			}else{
 				context.strokeStyle = '#00ff00';
-				players[i].locX += players[i].speed * xVector;
-				players[i].locY -= players[i].speed * yVector;
 			}
 			context.stroke();
-
 		}
-
-		checkForCollisions();
 		requestAnimationFrame(draw);
 	} //End DRAW FUNCTION
 
 	function getMousePosition(canvas, event){
 		var rect = canvas.getBoundingClientRect();
 		return {
-			x: Math.round((event.clientX-rect.left)/(rect.right-rect.left)*canvas.width*zoom),
-			y: Math.round((event.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height*zoom)
+			x: Math.round((event.clientX-rect.left)/(rect.right-rect.left)*canvas.width*player.zoom),
+			y: Math.round((event.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height*player.zoom)
 		};
 	}
 
-	function pointsMaker(num){
-		for(var i = 0; i < num; i++){
-			points.push(new Point());
-		}
-	}
-
-	function Point(){
-		this.color = getRandomColor();
-		this.locX = Math.floor((Math.random()*worldWidth) + 10); 
-		this.locY = Math.floor((Math.random()*worldHeight) + 10);
-		this.radius = 5;
-	}
-
-	function getRandomColor(){
-		var r = Math.floor((Math.random()*256)),
-			g = Math.floor((Math.random()*256)),
-			b = Math.floor((Math.random()*256));
-		return "rgb(" + r + "," + g + "," + b + ")";
-	}
-
-	function checkForCollisions(){
-		for(var i = 0; i < players.length; i++){
-			for(var j = 0; j < points.length; j++){
-			// AABB Test(square)
-				if(players[i].locX + players[i].radius + points[j].radius > points[j].locX 
-					&& players[i].locX < points[j].locX + players[i].radius + points[j].radius
-					&& players[i].locY + players[i].radius + points[j].radius > points[j].locY 
-					&& players[i].locY < points[j].locY + players[i].radius + points[j].radius){
-				// Pythagoras test(cricle)
-						distance = Math.sqrt(
-							((players[i].locX - points[j].locX) * (players[i].locX - points[j].locX)) + 
-							((players[i].locY - points[j].locY) * (players[i].locY - points[j].locY))	
-							);
-						if(distance < players[i].radius + points[j].radius){
-							players[i].color = points[j].color;
-							if(i == 0 && zoom > 1){
-								zoom -= .001;
-							}
-							points.splice(j, 1);
-							players[i].radius += 0.25;
-							if(players[i].speed < -0.005){
-								players[i].speed += 0.005;
-							}else if(players[i].speed > 0.005){
-								players[i].speed -= 0.005;
-							}
-							if(points.length < startPoints){
-								pointsMaker(1);
-							}
-						}
-				}
-			}
-			for(var k = 0; k < players.length; k++){
-				if(players[i] != players[k]){
-				// AABB Test
-					if(players[i].locX + players[i].radius + players[k].radius > players[k].locX 
-					&& players[i].locX < players[k].locX + players[i].radius + players[k].radius
-					&& players[i].locY + players[i].radius + players[k].radius > players[k].locY 
-					&& players[i].locY < players[k].locY + players[i].radius + players[k].radius){
-				// Pythagoras test
-						distance = Math.sqrt(
-							((players[i].locX - players[k].locX) * (players[i].locX - players[k].locX)) + 
-							((players[i].locY - players[k].locY) * (players[i].locY - players[k].locY))	
-							);
-						if(distance < players[i].radius + players[k].radius){
-							if(players[i].radius > players[k].radius){
-						// BOT DEATH
-								socketio.emit("message_to_server", {
-									message: "Bot Killed",
-									id: k,
-									killedBy: i,
-									radius: players[k].radius
-								});
-								players[i].radius += (players[k].radius * 0.25)
-								if(i == 0){
-									zoom -= (players[k].radius * 0.25) * .001;
-								}
-								players.splice(k, 1);
-							}else if(players[i].radius < players[k].radius){
-						// Player DEATH
-								socketio.emit("message_to_server", {
-									message: "PLAYER DEATH",
-									id: i,
-									killedBy: k,
-									radius: players[i].radius
-								});								
-								players[k].radius += (players[i].radius * 0.25)
-								zoom -= (players[i].radius * 0.25) * .01;
-								players.splice(i, 1)
-
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-
-	pointsMaker(startPoints);
-	newPlayer(startPlayers);
 	canvas.addEventListener("mousemove", function(event){
 		var mousePosition = getMousePosition(canvas, event);
 		var angleDeg = Math.atan2(mousePosition.y - (canvas.height/2), mousePosition.x - (canvas.width/2)) * 180 / Math.PI;
 		if(angleDeg >= 0 && angleDeg < 90){
-			xVector = 1 - (angleDeg/90);
-			yVector = -(angleDeg/90);
+			player.xVector = 1 - (angleDeg/90);
+			player.yVector = -(angleDeg/90);
 		}else if(angleDeg >= 90 && angleDeg <= 180){
-			xVector = -(angleDeg-90)/90;
-			yVector = -(1 - ((angleDeg-90)/90));
+			player.xVector = -(angleDeg-90)/90;
+			player.yVector = -(1 - ((angleDeg-90)/90));
 		}else if(angleDeg >= -180 && angleDeg < -90){
-			xVector = (angleDeg+90)/90;
-			yVector = (1 + ((angleDeg+90)/90));
+			player.xVector = (angleDeg+90)/90;
+			player.yVector = (1 + ((angleDeg+90)/90));
 		}else if(angleDeg < 0 && angleDeg >= -90){
-			xVector = (angleDeg+90)/90;
-			yVector = (1 - ((angleDeg+90)/90));
+			player.xVector = (angleDeg+90)/90;
+			player.yVector = (1 - ((angleDeg+90)/90));
 		}
-
-
 	}, false);
-	draw(); 
 //END CODE
 });
 
--- server/server.js
+++ server/server.js
@@ -2,28 +2,209 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-connections = [];
-
-app.use(express.static(__dirname + '/../client'));
-
-	io.sockets.on("connect", function(socket){
-	   console.log(socket.id)
-		connections.push(socket);
-		console.log('Connected: %s sockets connected', connections.length);
-
-		socket.on("message_to_server", function(data){
-			console.log(data);
-			io.sockets.emit("message_to_client",{
-				message: "Message Recieved, player " + data.id + " killed by player " + data.killedBy + " when he had radius of " + data.radius + "."  
-			});
-		});
-
-		socket.on('disconnect', function(data){
-			connections.splice(connections.indexOf(socket), 1);
-			console.log('Disconnected: %s sockets connected', connections.length);
-		});		
-	});
+var connections = [];
+var players = [];
+var orbs = [];
+var scoreboard = [];
+var defaultSpeed = 4;
+var defaultSize = 6;
+var defaultzoom = 1.5;
+var defaultOrbs = 1000;
+var worldWidth = 5000;
+var worldHeight = 5000;
+var tock;
 
 
 server.listen(process.env.port || 3333);
 console.log('server running on port 3333');
+
+app.use(express.static(__dirname + '/../client'));
+
+// BEGIN SOCKETS
+io.sockets.on("connect", function(socket){
+	if(connections == 0){
+		initGame();
+		console.log("New Field");
+	}
+	newPlayer();
+	function newPlayer(){
+		players.push(new Player());
+		socket.emit("init",{
+			init: players[players.length-1],
+			orbs: orbs,
+			players: players
+		});
+	}
+	function Player(){
+		this.id = (socket.id).substring(2);
+		this.locX = Math.floor((Math.random()*worldWidth) + 10); 
+		this.locY = Math.floor((Math.random()*worldHeight) + 10);
+		this.speed = defaultSpeed;
+		this.radius = defaultSize;
+		this.color = getRandomColor();
+		this.zoom = defaultzoom;
+		this.xVector = 0;
+		this.yVector = 0;
+		this.worldWidth = worldWidth;
+		this.worldHeight = worldHeight;
+	}
+
+	socket.on("tick", function(data){
+		movePlayer(data.player);
+		function movePlayer(player){
+			if(((player.locX < 5 && player.xVector < 0) || (player.locX > player.worldWidth) && (player.xVector > 0)) && ((player.locY < 5 && player.yVector > 0) || (player.locY > player.worldHeight) && (player.yVector < 0))){
+				console.log("can't move X or Y");
+			}else if((player.locX < 5 && player.xVector < 0) || (player.locX > player.worldWidth) && (player.xVector > 0)){
+				player.locY -= player.speed * player.yVector;
+				console.log("cant move X");
+			}else if((player.locY < 5 && player.yVector > 0) || (player.locY > player.worldHeight) && (player.yVector < 0)){
+				player.locX += player.speed * player.xVector;
+				console.log("cant move Y");
+			}else{
+				player.locX += player.speed * player.xVector;
+				player.locY -= player.speed * player.yVector;
+			}
+			for(var i = 0; i < players.length; i++){
+				if(players[i].id == player.id){
+					players[i].locX = player.locX;
+					players[i].locY = player.locY;
+				}
+			}
+		socket.emit("tock",{
+			players: players,
+			x: player.locX,
+			y: player.locY
+		});
+		checkForCollisions();
+		}
+	})
+	
+	function checkForCollisions(){
+		for(var i = 0; i < players.length; i++){
+			for(var j = 0; j < orbs.length; j++){
+			// AABB Test(square)
+				if(players[i].locX + players[i].radius + orbs[j].radius > orbs[j].locX 
+					&& players[i].locX < orbs[j].locX + players[i].radius + orbs[j].radius
+					&& players[i].locY + players[i].radius + orbs[j].radius > orbs[j].locY 
+					&& players[i].locY < orbs[j].locY + players[i].radius + orbs[j].radius){
+				// Pythagoras test(cricle)
+						distance = Math.sqrt(
+							((players[i].locX - orbs[j].locX) * (players[i].locX - orbs[j].locX)) + 
+							((players[i].locY - orbs[j].locY) * (players[i].locY - orbs[j].locY))	
+							);
+						if(distance < players[i].radius + orbs[j].radius){
+							players[i].color = orbs[j].color;
+							if(i == 0 && players[i].zoom > 1){
+								players[i].zoom -= .001;
+							}
+							orbs.splice(j, 1);
+							players[i].radius += 0.25;
+							if(players[i].speed < -0.005){
+								players[i].speed += 0.005;
+							}else if(players[i].speed > 0.005){
+								players[i].speed -= 0.005;
+							}
+							if(orbs.length < defaultOrbs){
+								createOrbs(1);
+							}
+							socket.emit("collision",{
+								players: players,
+								orbs: orbs
+							});
+						}
+				}
+			}
+			// for(var k = 0; k < players.length; k++){
+			// 	if(players[i] != players[k]){
+			// 	// AABB Test
+			// 		if(players[i].locX + players[i].radius + players[k].radius > players[k].locX 
+			// 		&& players[i].locX < players[k].locX + players[i].radius + players[k].radius
+			// 		&& players[i].locY + players[i].radius + players[k].radius > players[k].locY 
+			// 		&& players[i].locY < players[k].locY + players[i].radius + players[k].radius){
+			// 	// Pythagoras test
+			// 			distance = Math.sqrt(
+			// 				((players[i].locX - players[k].locX) * (players[i].locX - players[k].locX)) + 
+			// 				((players[i].locY - players[k].locY) * (players[i].locY - players[k].locY))	
+			// 				);
+			// 			if(distance < players[i].radius + players[k].radius){
+			// 				if(players[i].radius > players[k].radius){
+			// 			// BOT DEATH
+			// 					socket.emit("message_to_server", {
+			// 						message: "Bot Killed",
+			// 						id: k,
+			// 						killedBy: i,
+			// 						radius: players[k].radius
+			// 					});
+			// 					players[i].radius += (players[k].radius * 0.25)
+			// 					if(i == 0){
+			// 						player.zoom -= (players[k].radius * 0.25) * .001;
+			// 					}
+			// 					players.splice(k, 1);
+			// 				}else if(players[i].radius < players[k].radius){
+			// 			// Player DEATH
+			// 					socket.emit("message_to_server", {
+			// 						message: "PLAYER DEATH",
+			// 						id: i,
+			// 						killedBy: k,
+			// 						radius: players[i].radius
+			// 					});								
+			// 					players[k].radius += (players[i].radius * 0.25)
+			// 					player.zoom -= (players[i].radius * 0.25) * .01;
+			// 					players.splice(i, 1)
+			// 				}
+			// 			}
+			// 		}
+			// 	}
+			// }
+		}
+	}
+
+	// connections.push(socket);
+	console.log('Connected: %s sockets connected', players.length);
+
+	socket.on("message_to_server", function(data){
+		console.log(data);
+		io.sockets.emit("message_to_client",{
+			message: "Message Recieved, player " + data.id + " killed by player " + data.killedBy + " when he had radius of " + data.radius + "."  
+		});
+	});
+
+	socket.on('disconnect', function(data){
+		// connections.splice(connections.indexOf(socket), 1);
+		for(var i = 0; i < players.length; i++){
+			if(players[i].id == (socket.id).substring(2)){
+				players.splice(i, 1);
+			}
+		}
+		console.log('Disconnected: %s sockets connected', players.length);
+	});	
+
+// END SOCKETS	
+});
+
+// Starting a new game
+function initGame(){
+	orbs = [];
+	createOrbs(defaultOrbs);
+}
+
+function createOrbs(num){
+	for(var i = 0; i < num; i++){
+		orbs.push(new Orb());
+	}
+}
+
+function Orb(){
+	this.color = getRandomColor();
+	this.locX = Math.floor((Math.random()*worldWidth) + 10); 
+	this.locY = Math.floor((Math.random()*worldHeight) + 10);
+	this.radius = 5;
+}
+
+function getRandomColor(){
+	var r = Math.floor((Math.random()*206)+50),
+		g = Math.floor((Math.random()*206)+50),
+		b = Math.floor((Math.random()*206)+50);
+	return "rgb(" + r + "," + g + "," + b + ")";
+}
+
